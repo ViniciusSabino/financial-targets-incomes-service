ce9a542c80799660ba249a37ce29cc989140a2fa
Vinicius Rocha <vnc.chelsea@gmail.com>
2025-08-27 16:32:10 -0300

    doc: refactor OpenAPIConfig folder

--- src/main/java/com/financialtargets/incomes/infrastructure/config/OpenAPIConfig.java
+++ src/main/java/com/financialtargets/incomes/infrastructure/config/OpenAPIConfig.java
@@ -1,4 +1,4 @@
-package com.financialtargets.incomes.config;
+package com.financialtargets.incomes.infrastructure.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
