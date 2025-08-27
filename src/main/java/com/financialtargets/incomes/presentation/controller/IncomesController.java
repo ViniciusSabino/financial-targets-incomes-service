fc3e12a42ff3ff98b5e350d80e95977cde4bac12
Vinicius Rocha <vnc.chelsea@gmail.com>
2025-08-27 16:22:31 -0300

    doc: first implementation open api swagger

--- src/main/java/com/financialtargets/incomes/presentation/controller/IncomesController.java
+++ src/main/java/com/financialtargets/incomes/presentation/controller/IncomesController.java
@@ -3,12 +3,53 @@ package com.financialtargets.incomes.presentation.controller;
 import com.financialtargets.incomes.application.dto.IncomeCreateDTO;
 import com.financialtargets.incomes.application.dto.IncomeDTO;
 import com.financialtargets.incomes.domain.exception.IncomeException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
+@Tag(name = "Incomes", description = "Endpoints from Managing Incomes")
 public interface IncomesController {
+
+    @Operation(summary = "Create a Income",
+            description = "Create a Income",
+            tags = {"Incomes"},
+            responses = {
+                    @ApiResponse(description = "Created", responseCode = "201", content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = IncomeDTO.class)
+                            )
+                    }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
     ResponseEntity<IncomeDTO> create(IncomeCreateDTO incomeCreateDTO) throws IncomeException;
 
+    @Operation(summary = "List Incomes by month and year parameter",
+            description = "List Incomes by month and year parameter",
+            tags = {"Incomes"},
+            responses = {
+                    @ApiResponse(description = "Success", responseCode = "200", content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    array = @ArraySchema(schema = @Schema(implementation = IncomeDTO.class))
+                            )
+                    }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
     ResponseEntity<List<IncomeDTO>> listByMonth(String month, String year);
 }
--- src/main/java/com/financialtargets/incomes/presentation/controller/SummaryController.java
+++ src/main/java/com/financialtargets/incomes/presentation/controller/SummaryController.java
@@ -1,8 +1,32 @@
 package com.financialtargets.incomes.presentation.controller;
 
 import com.financialtargets.incomes.application.dto.IncomesSummaryDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
+@Tag(name = "Summary", description = "Endpoints from Summary Incomes")
 public interface SummaryController {
+
+    @Operation(summary = "Get Summary Incomes",
+            description = "Get Summary Incomes",
+            tags = {"Summary"},
+            responses = {
+                    @ApiResponse(description = "OK", responseCode = "200", content = {
+                            @Content(
+                                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                                    schema = @Schema(implementation = IncomesSummaryDTO.class)
+                            )
+                    }),
+                    @ApiResponse(description = "Bad Request", responseCode = "400", content = @Content),
+                    @ApiResponse(description = "Unauthorized", responseCode = "401", content = @Content),
+                    @ApiResponse(description = "Not Found", responseCode = "404", content = @Content),
+                    @ApiResponse(description = "Internal Server Error", responseCode = "500", content = @Content)
+            }
+    )
     ResponseEntity<IncomesSummaryDTO> getSummary(String month, String year);
 }
